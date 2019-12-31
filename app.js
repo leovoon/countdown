5af582315a5e6f2fdd2489f405994c72fc34692a
Leo Voon <voonlihhaur@gmail.com>
2019-12-31 17:58:35 +0800

    countdown 2020

---
+++ app.js
@@ -0,0 +1,77 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+    const newyear = document.getElementsByClassName('hooray')[0];
+if (hours === 0 && minutes === 0 && seconds === 0){
+
+    newyear.classList.add('fade');
+
+}
+}, 1000);
+
+
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector('#hitokoto')
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
+
+
+document.getElementById('getAPI').addEventListener('click', () => {
+
+
+    fetch('https://v1.hitokoto.cn')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (data) {
+      var hitokoto = document.getElementById('hitokoto');
+      
+      animateCSS('hitokoto', 'fadeIn')
+
+
+      hitokoto.innerText = data.hitokoto; 
+
+    })
+    .catch(function (err) {
+      console.error(err);
+    })
+
+    
+});
+
+
